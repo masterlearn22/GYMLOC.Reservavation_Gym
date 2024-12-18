333fe35955382dbd7a0b0e845bd88ff8c4c0af2e
nerhays <sfa.asus@gmail.com>
2024-12-18 17:13:41 +0700

    ROUTE ROUTE

--- routes/web.php
+++ routes/web.php
@@ -12,17 +12,16 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TransaksiController;
 use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/anjay', function () {
     return view('welcome');
 });
 
-//Dashboard Page
-Route::get('/', function () {
-    return view('IntroWebsite.index');
-});
 
-
+Route::get('/', [DashboardController::class, 'index'])->name('dasboard.index');
+Route::get('/index', [DashboardController::class, 'index'])->name('dasboard.login');
+Route::get('/tentangkami', [AboutController::class, 'index'])->name('about.index');
 // Route untuk AuthController (Login, Logout, Register)
 Route::post('/simpanregist', [AuthController::class, 'Registrasi'])->name('register');
 Route::get('/login', [AuthController::class, 'Tampilanlogin']);
@@ -39,10 +38,6 @@ Route::post('/reservations/store', [ReservationController::class, 'create']);
 Route::put('/reservations/{id}/status', [ReservationController::class, 'updateStatus']);
 Route::post('/reservations/{id}/payment', [ReservationController::class, 'addPayment']);
 
-
-Route::get('/index', function () {
-    return view('IntroWebsite.index');
-});
 
 
 Route::get('/gyms', [GymController::class, 'index']);
